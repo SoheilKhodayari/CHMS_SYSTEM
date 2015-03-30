be80505d58711647ab703103a553e700ba992193
SoheilKhodayari <s_khodayari@outlook.com>
2015-03-30 16:27:56 +0430

    Update models.py

--- history_records/medical_history/models.py
+++ history_records/medical_history/models.py
@@ -1,58 +1,5 @@
 from django.db import models
 from patient.models import Patient
-from history_records.surgical_history.models import SurgeryRecord
-class MedicalHistory(models.Model):
-
-    """
-      This defines the Medical History that the patient has had .
-    """
-
-    def __init__(self, *args, **kwargs):
-      super(MedicalHistory,self).__init__(*args, **kwargs)
-      self.__model_label__ = "medical_history"
-      self._parent_model = 'patient'
-
-    had_infectious_disease = models.BooleanField(default=None)
-    had_allergic_disease = models.BooleanField(default = None)
-    pregnancy_warning = models.BooleanField(default = None)
-
-    diseases=models.ManyToManyField(Disease) #To add : MedicalHistoryInstance.diseases.add(Disease_Instance)
-    patient_detail = models.OneToOneField(Patient) # surgery records are connected accordingly,
-
-    patient_current_status = models.TextField("Status",
-                              max_length=500,
-                              null=True,
-                              blank=True
-                              )
-
-    def __unicode__(self):
-        return "%s" % (self.patient_detail)
-
-class Disease(models.Model):
-    def __init__(self, *args, **kwargs):
-            super(Disease,self).__init__(*args, **kwargs)
-            self.__model_label__ = "disease"
-            self._parent_model = 'MedicalHistory'
-
-    disease = models.CharField(max_length=100)
-    severity = models.CharField(max_length=100)
-    date_of_diagnosis = models.DateField(auto_now_add=False, null=True,blank=True)
-    is_active_ = models.BooleanField("Active?",default=None)
-    remarks = models.TextField(max_length=1000,
-                               help_text="Any Other Remarks",
-                               default="None"
-                               )
-    #icd 10 is the universal classification of diseases
-    icd_10  = models.CharField("ICD 10", max_length=100,null=True, blank=True)
-
-    medical_History=models.ForeignKey(MedicalHistory)
-
-    disease_medication_list=models.OneToOneField(MedicationList)
-
-    def __unicode__(self):
-        return self.icd_10+ ","+self.disease
-
-
 class MedicationList(models.Model):
 
     """
@@ -84,3 +31,53 @@ class MedicationList(models.Model):
 
     def __unicode__(self):
         return "%s" % (self.medication)
+
+class Disease(models.Model):
+    def __init__(self, *args, **kwargs):
+            super(Disease,self).__init__(*args, **kwargs)
+            self.__model_label__ = "disease"
+            self._parent_model = 'MedicalHistory'
+
+    disease = models.CharField(max_length=100)
+    severity = models.CharField(max_length=100)
+    date_of_diagnosis = models.DateField(auto_now_add=False, null=True,blank=True)
+    is_active = models.BooleanField("Active?",default=None)
+    remarks = models.TextField(max_length=1000,
+                               help_text="Any Other Remarks",
+                               default="None"
+                               )
+    #icd 10 is the universal classification of diseases
+    icd_10  = models.CharField("ICD 10", max_length=100,null=True, blank=True)
+
+    #medical_History=models.ForeignKey(MedicalHistory)
+
+    disease_medication_list=models.OneToOneField(MedicationList)
+
+class MedicalHistory(models.Model):
+
+    """
+      This defines the Medical History that the patient has had .
+    """
+
+    def __init__(self, *args, **kwargs):
+      super(MedicalHistory,self).__init__(*args, **kwargs)
+      self.__model_label__ = "medical_history"
+      self._parent_model = 'patient'
+
+    had_infectious_disease = models.BooleanField(default=None)
+    had_allergic_disease = models.BooleanField(default = None)
+    pregnancy_warning = models.BooleanField(default = None)
+
+    diseases=models.ManyToManyField(Disease) #To add : MedicalHistoryInstance.diseases.add(Disease_Instance)
+    patient_detail = models.OneToOneField(Patient) # surgery records are connected accordingly,
+
+    patient_current_status = models.TextField("Status",
+                              max_length=500,
+                              null=True,
+                              blank=True
+                              )
+
+    def __unicode__(self):
+        return "%s" % (self.patient_detail)
+
+
