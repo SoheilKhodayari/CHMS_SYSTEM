b8da5206b02045c942aef9841d8621a4a3133b3d
SoheilKhodayari <s_khodayari@outlook.com>
2015-05-21 23:19:42 +0430

    exception handled

--- history_records/views.py
+++ history_records/views.py
@@ -11,32 +11,45 @@ def index(request,username):
 
 
 def medical_history(request,username):
-    patient=Patient.objects.get(user__username=username)
-    history=patient.medical_file.get().medical_history_sheet
-    return render(request,'medical_history/medical_history_sheet.html',{'patient':patient,'sheet':history})
+    try:
+        patient=Patient.objects.get(user__username=username)
+        history=patient.medical_file.get().medical_history_sheet
+        return render(request,'medical_history/medical_history_sheet.html',{'patient':patient,'sheet':history})
+    except:
+        return HttpResponse("Currently No Records")
 
 
 
 
 
 def progress_notes(request,username):
-    patient=Patient.objects.get(user__username=username)
-    progress_note_sheet=patient.medical_file.get().progress_note_sheet
-    return render(request,'medical_history/progress_notes_sheet.html',{'patient':patient,'sheet':progress_note_sheet})
+    try:
+        patient=Patient.objects.get(user__username=username)
+        progress_note_sheet=patient.medical_file.get().progress_note_sheet
+        return render(request,'medical_history/progress_notes_sheet.html',{'patient':patient,'sheet':progress_note_sheet})
+    except:
+        return HttpResponse("Currently No Records")
 
 
 
 def physician_orders(request,username):
-    patient=Patient.objects.get(user__username=username)
-    orders=patient.medical_file.get().physician_order_sheet
+    try:
+        patient=Patient.objects.get(user__username=username)
+        orders=patient.medical_file.get().physician_order_sheet
 
-    return render(request,'medical_history/physician_oder_sheet.html',{'patient':patient,'sheet':orders})
+        return render(request,'medical_history/physician_oder_sheet.html',{'patient':patient,'sheet':orders})
+    except:
+        return HttpResponse("Currently No Records")
 
 
 
 
 def unit_summary(request,username):
-    patient=Patient.objects.get(user__username=username)
-    unit_summary_sheet=patient.medical_file.get().unit_summary_sheet
+    try:
+        
+        patient=Patient.objects.get(user__username=username)
+        unit_summary_sheet=patient.medical_file.get().unit_summary_sheet
 
-    return render(request,'medical_history/unit_summary_sheet.html',{'patient':patient,'sheet':unit_summary_sheet})
+        return render(request,'medical_history/unit_summary_sheet.html',{'patient':patient,'sheet':unit_summary_sheet})
+    except:
+        return HttpResponse("Currently No Records")
