f05a32a5297a717581ee84431952e73aa0683b75
Amir.zsh <amirshahabi1123@gmail.com>
2015-06-10 08:29:18 +0430

    history edit template

--- history_records/views.py
+++ history_records/views.py
@@ -14,6 +14,7 @@ def medical_history(request,username):
     try:
         patient=Patient.objects.get(user__username=username)
         history=patient.medical_file.get().medical_history_sheet
+
         return render(request,'medical_history/medical_history_sheet.html',{'patient':patient,'sheet':history})
     except:
         return HttpResponse("Currently No Records")
@@ -51,5 +52,63 @@ def unit_summary(request,username):
         unit_summary_sheet=patient.medical_file.get().unit_summary_sheet
 
         return render(request,'medical_history/unit_summary_sheet.html',{'patient':patient,'sheet':unit_summary_sheet})
+    except:
+        return HttpResponse("Currently No Records")
+
+def edit_unit_summary(request,username):
+    try:
+
+        patient=Patient.objects.get(user__username=username)
+        unit_summary_sheet=patient.medical_file.get().unit_summary_sheet
+        unit_summary_sheet.chief_complaint_and_primary_diagnosis=request.POST['ccapd']
+        unit_summary_sheet.disease_progress=request.POST['dp']
+        unit_summary_sheet.final_diagnosis=request.POST['fd']
+        unit_summary_sheet.medical_and_surgical_procedures=request.POST['masp']
+        unit_summary_sheet.patient_condition_on_discharge=request.POST['pd']
+        unit_summary_sheet.recommendations_after_discharge=request.POST['ra']
+        unit_summary_sheet.results_of_paraclinical_examinations=request.POST['rope']
+        unit_summary_sheet.save()
+        return render(request,'medical_history/unit_summary_sheet.html',{'patient':patient,'sheet':unit_summary_sheet})
+    except:
+        return HttpResponse("Currently No Records")
+
+
+
+
+def edit_medical_history(request,username):
+    try:
+        patient=Patient.objects.get(user__username=username)
+        history=patient.medical_file.get().medical_history_sheet
+        history.allergy_to=request.POST['at']
+        history.chief_complain=request.POST['cf']
+        history.current_drug_theraphy_and_other_addiction=request.POST['crtoa']
+        history.family_history=request.POST['fh']
+        history.pass_diseases_history=request.POST['phd']
+        history.primary_dx=request.POST['pdx']
+        history.summary=request.POST['summary']
+        history.history_of_present_illness=request.POST['hopi']
+        return render(request,'medical_history/medical_history_sheet.html',{'patient':patient,'sheet':history})
+    except:
+        return HttpResponse("Currently No Records")
+
+
+def edit_physician_orders(request,username):
+    try:
+        patient=Patient.objects.get(user__username=username)
+        orders=patient.medical_file.get().physician_order_sheet
+        new_order=Order(date=request.POST['date'],time=request.POST['time'],order=request.POST['order'],user=request.user,sheet=orders)
+        new_order.save()
+        return render(request,'medical_history/physician_oder_sheet.html',{'patient':patient,'sheet':orders})
+    except:
+        return HttpResponse("Currently No Records")
+
+def edit_progress_notes(request,username):
+    try:
+        patient=Patient.objects.get(user__username=username)
+        progress_note_sheet=patient.medical_file.get().progress_note_sheet
+        user=request.user
+        new_progress_note=Progress_note(date=request.POST['date'],treatment_progress=request.POST['tp'],sheet=progress_note_sheet,physician=user)
+        new_progress_note.save()
+        return render(request,'medical_history/progress_notes_sheet.html',{'patient':patient,'sheet':progress_note_sheet})
     except:
         return HttpResponse("Currently No Records")
